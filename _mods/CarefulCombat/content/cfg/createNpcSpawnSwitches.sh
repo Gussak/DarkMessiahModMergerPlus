3fe5b364d10aebeb5ac243243a1ba4cbfce1c34b
gussak <ghussack@gmail.com>
2026-07-16 23:22:18 -0300

    fix/improve/doc

--- _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
+++ _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
@@ -41,16 +41,16 @@ astr=(
 	#mm_npc_create_human_guard_bow #this is friendly right?
 	#mm_npc_create_human_guard_shield #this is friendly right?
 	mm_npc_create_lich
-	#mm_npc_create_lich_king
+	mm_npc_create_lich_king
 	#mm_npc_create_necro_guard
 	mm_npc_create_necro_guard_bow # good because they wont drop arrows and wont make it easier
 	mm_npc_create_necro_guard_shield # this would drop the shield I guess, use just one per room and only if you have no shield (tho they seem to fight better? but still very weak against lethal things like fire, drowning etc)
 	mm_npc_create_necromancer
-	#mm_npc_create_necromancer_lord
+	mm_npc_create_necromancer_lord
 	#mm_npc_create_orc_sword
 	mm_npc_create_orc_sword_bow
 	mm_npc_create_orc_sword_shield
-	#mm_npc_create_servant_specter
+	mm_npc_create_servant_specter
 	mm_npc_create_spider
 	#mm_npc_create_spider_mini
 	mm_npc_create_undead
