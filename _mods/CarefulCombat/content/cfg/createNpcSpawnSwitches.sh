b62b1829f466beae532d82f2b7ac4d8e386b69cc
gussak <ghussack@gmail.com>
2026-07-21 17:14:47 -0300

    fixes

--- _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
+++ _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
@@ -54,7 +54,7 @@ astrNPC=(
 	mm_npc_create_orc_sword_shield
 	mm_npc_create_servant_specter
 	mm_npc_create_spider
-	#mm_npc_create_spider_mini #do not use this one, it will encumber AI and RAM too that is better for challenging NPCs
+	#mm_npc_create_spider_mini #do not use this one, it will encumber AI and RAM too that is better for challenging NPCs.
 	mm_npc_create_undead
 	#mm_npc_create_villager_undead
 	#mm_npc_create_wizard #this is friendly right?
@@ -112,7 +112,7 @@ if [[ "${1-}" == "-m" ]];then #help read last condump and prepare a cfg file to 
 	fi
 	echo -n >"$strMapCfgFile"
 	
-	strPosRestore="$(ugrep "${strRegexMapPos}" "$strFlCondump" |sed -r -e "s@${strRegexMapPos}@\2@g" |tr -d 'xyz,\r')"
+	strPosRestore="$(ugrep "${strRegexMapPos}" "$strFlCondump" |head -n 1 |sed -r -e "s@${strRegexMapPos}@\2@g" |tr -d 'xyz,\r')"
 	if [[ -z "$strPosRestore" ]];then
 		FUNCechoInfo "[ERROR:noPosToRestoreDetected]"
 		FUNCexit 1
@@ -133,7 +133,7 @@ if [[ "${1-}" == "-m" ]];then #help read last condump and prepare a cfg file to 
 	fi
 	FUNCechoAndFillFile "// AUTO GENERATED WITH $(basename "$0")"
 	FUNCechoAndFillFile "// FILL MAP WITH NPCs, total $nTot"
-	FUNCechoAndFillFile "alias gskCCnpcSpawn_helper \"developer 1; gskEffect100; host_timescale 0.01; ai_disable 1\""
+	FUNCechoAndFillFile "alias gskCCnpcSpawn_helper \"developer 1; gskEffect100; host_timescale 0.01; ai_disable\""
 	FUNCechoAndFillFile "alias gskCCnpcSpawn_next gskCCnpcSpawn_000"
 	#for((i=0;i<${#astrSpawnHintList[@]};i+=iDataLines));do
 	for((i=0;i<${#astrAllLines[@]};i++));do
@@ -176,7 +176,7 @@ if [[ "${1-}" == "-m" ]];then #help read last condump and prepare a cfg file to 
 			i=$iLnData
 		fi
 	done
-	FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCountPlus1} \"echo FinishedSpawnings; gskEffectOFF; play *arkane/english/xana/l05_xana_pillardone.wav; ${strRestorePosInTheEnd}; host_timescale 1.0; ai_disable 0; developer 0; \"" #this also prevents continuing thru some previous list entries of a previous test run or map may be
+	FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCountPlus1} \"echo FinishedSpawnings; gskEffectOFF; play *arkane/english/xana/l05_xana_pillardone.wav; ${strRestorePosInTheEnd}; host_timescale 1.0; ai_disable; developer 0; \"" #this also prevents continuing thru some previous list entries of a previous test run or map may be
 	
 	if egrep "remove" -i "${strMapCfgFile}.condump.txt";then
 		FUNCechoInfo "[WARN] removes detected, better edit to remove from that line up to 'gskSpawnHint' and rerun!" #TODO this can be scripted easily if the echo on the console is like: RemoveAbove=1 or Remove=1 or RM1; echo RM1; echo rm2
