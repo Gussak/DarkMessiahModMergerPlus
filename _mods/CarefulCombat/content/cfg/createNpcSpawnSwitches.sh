5375dfde03cb14e5477c2ad026d2c19bf74d937b
gussak <ghussack@gmail.com>
2026-07-21 12:12:07 -0300

    fixes

--- _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
+++ _mods/CarefulCombat/content/cfg/createNpcSpawnSwitches.sh
@@ -124,7 +124,7 @@ if [[ "${1-}" == "-m" ]];then #help read last condump and prepare a cfg file to 
 	fi
 	FUNCechoAndFillFile "// AUTO GENERATED WITH $(basename "$0")"
 	FUNCechoAndFillFile "// FILL MAP WITH NPCs, total $nTot"
-	FUNCechoAndFillFile "alias gskCCnpcSpawn_helper \"gskEffect100; host_timescale 0.01; ai_disable 1\""
+	FUNCechoAndFillFile "alias gskCCnpcSpawn_helper \"developer 1; gskEffect100; host_timescale 0.01; ai_disable 1\""
 	FUNCechoAndFillFile "alias gskCCnpcSpawn_next gskCCnpcSpawn_000"
 	#for((i=0;i<${#astrSpawnHintList[@]};i+=iDataLines));do
 	for((i=0;i<${#astrAllLines[@]};i++));do
@@ -161,13 +161,13 @@ if [[ "${1-}" == "-m" ]];then #help read last condump and prepare a cfg file to 
 			strCount="$(      printf %03d $((iCount  )) )"
 			strCountPlus1="$( printf %03d $((iCount+1)) )"
 			
-			FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCount} \"${strPos}; ${strNPC}; gskCCnpcSpawn_helper; alias gskCCnpcSpawn_next gskCCnpcSpawn_${strCountPlus1}\""
+			FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCount} \"${strPos}; ${strNPC}; echo ${iCount}/${nTot}; gskCCnpcSpawn_helper; alias gskCCnpcSpawn_next gskCCnpcSpawn_${strCountPlus1}\""
 			((iCount++))&&:
 			
 			i=$iLnData
 		fi
 	done
-	FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCountPlus1} \"echo FinishedSpawnings; gskEffectOFF; play *arkane/english/xana/l05_xana_pillardone.wav; ${strRestorePosInTheEnd}; host_timescale 1.0; ai_disable 0\"" #this also prevents continuing thru some previous list entries of a previous test run or map may be
+	FUNCechoAndFillFile "alias gskCCnpcSpawn_${strCountPlus1} \"echo FinishedSpawnings; gskEffectOFF; play *arkane/english/xana/l05_xana_pillardone.wav; ${strRestorePosInTheEnd}; host_timescale 1.0; ai_disable 0; developer 0; \"" #this also prevents continuing thru some previous list entries of a previous test run or map may be
 else # create spawner aliases
 	echo
 	echo "// Total ${#astrNPC[@]} NPCs"
