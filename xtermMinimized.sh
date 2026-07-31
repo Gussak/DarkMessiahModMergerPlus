356fef4189f1aff3a54130e1af43a26a1817e569
gussak <ghussack@gmail.com>
2026-07-31 15:38:43 -0300

    wip

---
+++ xtermMinimized.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+strTitle="$1";shift
+aParams=("$@")
+
+if pgrep -fa "$strTitle";then
+	echo "[ERROR] strTitle='$strTitle' must be unique but it is already running..."
+	exit 1
+fi
+
+pkill -SIGKILL -fe "$strTitle"
+(xterm -title "$strTitle" "${aParams[@]}" & disown)
+while true;do
+	pkill -SIGSTOP -fe "$strTitle";
+	if wmctrl -l |grep "$strTitle";then
+		echo "[INFO] window is ready"
+		xdotool windowminimize "$(wmctrl -l |grep "$strTitle" |awk '{print $1}')"
+		pkill -SIGCONT -fe "$strTitle";
+	else
+		pkill -SIGCONT -fe "$strTitle";
+	fi
+done
