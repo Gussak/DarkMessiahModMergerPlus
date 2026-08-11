048fd43b2bfa6b456ff140037aa959692a8f6fe1
gussak <ghussack@gmail.com>
2026-08-10 23:09:13 -0300

    fix

--- _mods/000gskBaseLib/content/cfg/createAliasAndErasers.sh
+++ _mods/000gskBaseLib/content/cfg/createAliasAndErasers.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 #we cant ex.: alias gskHurtMeFUNC hurtme, then use gskHurtMeFUNC 3, it wont work
-for((i=1;i<=100;i++));do
-	echo "alias gskHurtme$(printf %03d $i) \"hurtme $i\""
-done
 
 # line limit is 1024 chars
 function FUNCerasers() {
-	local lstrEraser="alias gskHMHurtmeEraser1of3 \""
-	for((i=$1;i<=$2;i++));do
-		lstrEraser+="alias gskHurtme$(printf %03d $i);"
+	local lstrMainEraser="$1";shift
+	local lstrToEraseBN="$1";shift
+	local lnFrom="$1";shift
+	local lnTo="$1";shift
+	
+	local lstrEraser="alias ${lstrMainEraser} \""
+	for((i=lnFrom;i<=lnTo;i++));do
+		lstrEraser+="alias ${lstrToEraseBN}$(printf %03d $i);"
 	done
 	lstrEraser+="\""
 	echo "$lstrEraser"
 }
-FUNCerasers 1 33
-FUNCerasers 34 66
-FUNCerasers 67 100
+
+echo
+for((i=1;i<=100;i++));do
+	echo "alias gskHurtme$(printf %03d $i) \"hurtme $i\""
+done
+FUNCerasers "gskHMHurtmeEraser1of3" "gskHurtme" 1  33
+FUNCerasers "gskHMHurtmeEraser2of3" "gskHurtme" 34  66
+FUNCerasers "gskHMHurtmeEraser3of3" "gskHurtme" 67 100
+
+################
+echo
+for((i=1;i<=10;i++));do
+	echo "alias gskManaReg$(printf %03d $i) \"mm_player_time_to_add_mana 0.$(printf %02d $i)\""
+done
+FUNCerasers "gskManaRegEraser" "gskManaReg" 1 10
--- _mods/MultiplyEnemies/content/cfg/createNpcSpawnSwitches.sh
+++ _mods/MultiplyEnemies/content/cfg/createNpcSpawnSwitches.sh
@@ -347,8 +347,8 @@ if $bCreateSpawnsForCurrentMap;then
 	FUNCechoAndFillFile "// AUTO GENERATED WITH $(basename "$0"). DO NOT PATCH! Patch the '${strFlCondumpClean}' file instead!"
 	FUNCechoAndFillFile "// FILL MAP WITH NPCs, total $nTotSpawns"
 	#FUNCechoAndFillFile "alias gskCCnpcSpawn_helper \"\""
-	strCmdsON=" gskEchoOn; +duck; gskDevGodModeToggles; gskEffect100 " # do not use host_timescale 0.01 as it will mess teleporting. +duck is to help to fit yourself in smaller places causing less issues
-	strCmdsOFF=" -duck; gskDevGodModeToggles; gskEffectOFF; gskEchoOff "
+	strCmdsON=" gskEchoOn; +duck; gskDevGodModeToggles; gskEffect100; gskManaRegEraser; gskHMHurtmeEraser1of3; gskHMHurtmeEraser2of3; gskHMHurtmeEraser3of3 " # do not use host_timescale 0.01 as it will mess teleporting. +duck is to help to fit yourself in smaller places causing less issues
+	strCmdsOFF=" -duck; gskDevGodModeToggles; gskEffectOFF; gskEchoOff; exec gskBaseLib " # gskbaselib is to restore what was erased to avoid messing the player HP and Mana pools
 	FUNCechoAndFillFile "alias gskCCnpcSpawn_next \"${strCmdsON}; gskCCnpcSpawn_$( printf %03d $((iSpawnCount)) )\"" # initializes with some dev toggles
 	#for((i=0;i<${#astrSpawnHintList[@]};i+=iDataLines));do
 	for((i=0;i<${#astrAllLines[@]};i++));do
