b00846e901967a4df59f8aef14e108ff34b1215b
gussak <ghussack@gmail.com>
2026-08-14 23:06:35 -0300

    improve

--- _mods/000gskBaseLib/content/cfg/gskReloadCfgs.sh
+++ _mods/000gskBaseLib/content/cfg/gskReloadCfgs.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+#TODO: initial alias selections could all be in a new cfg file that would not be part of the loaded ones like ..._AliasSelectorsInitializer.cfg
+
+astrSkip=(
+	"auto_load_last_quicksave"
+	"gskdevhelpDoNotEnable"
+	"gskmap_.*"
+	"gskShowAllKeyBindings"
+	"unlimitededition"
+	"pause_after_load"
+	"DoNotEnable"
+	"_AliasSelectorsInitializer.cfg"
+); strSkipRegex="$(echo "${astrSkip[@]}" |tr ' ' '|')"; declare -p strSkipRegex
+
 #helper 'q' reload some cfgs
-(cd *993*;find -iregex '.*cfg/.*.cfg') |egrep -v 'SUCCESS|game.cfg|/cfg$' |sed -r -e 's@.*/cfg/(.*)[.]cfg$@ exec \1@g' |sort -u |egrep -v 'auto_load_last_quicksave|gskdevhelpDoNotEnable|gskmap_.*|gskShowAllKeyBindings|unlimitededition|pause_after_load|DoNotEnable' |tr '\n' ';' |sed -r -e 's@.*@alias +gskReloadCfgs "gskEchoOn; contimes 50; & echo ReloadedSomeCFGs; "@g'
+while ! cd *GSK_ModMerger_AndMiniMods*;do cd ..;done
+(find -iregex '.*cfg/.*.cfg') \
+	|egrep -v 'SUCCESS|game.cfg|/cfg$' \
+	|sed -r -e 's@.*/cfg/(.*)[.]cfg$@ exec \1@g' \
+	|sort -u \
+	|egrep -v "${strSkipRegex}" \
+	|tr '\n' ';' \
+	|sed -r -e 's@.*@alias +gskReloadCfgs "gskEchoOn; contimes 50; & echo ReloadedSomeCFGs; "@g'
