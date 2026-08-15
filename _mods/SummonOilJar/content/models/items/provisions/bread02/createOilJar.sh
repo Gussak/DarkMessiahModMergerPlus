13a08d068f2471214998bd5c931229653c85b549
gussak <ghussack@gmail.com>
2026-08-15 19:48:11 -0300

    new spawnable oil jar

---
+++ _mods/SummonOilJar/content/models/items/provisions/bread02/createOilJar.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#	BSD 3-Clause License
+#
+#	Copyright (c) 2026, Gussak<https://github.com/Gussak>
+#
+#	Redistribution and use in source and binary forms, with or without
+#	modification, are permitted provided that the following conditions are met:
+#
+#	1. Redistributions of source code must retain the above copyright notice, this
+#		 list of conditions and the following disclaimer.
+#
+#	2. Redistributions in binary form must reproduce the above copyright notice,
+#		 this list of conditions and the following disclaimer in the documentation
+#		 and/or other materials provided with the distribution.
+#
+#	3. Neither the name of the copyright holder nor the names of its
+#		 contributors may be used to endorse or promote products derived from
+#		 this software without specific prior written permission.
+#
+#	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+while [[ ! -f "./allMergerScriptsGenericConfig.sh" ]];do cd ..;done; source "./allMergerScriptsGenericConfig.sh"; FUNCminiModInit "$@"
+
+strPath="${strVanillaAllExtractedFilesPath}/mm/models/props/furnitures/gob/l6_jar_oil/"
+
+if [[ ! -f "${strPath}/l6_jar_oil.mdl" ]];then
+	FUNCechoInfo "[ERROR] file '${strPath}/l6_jar_oil.mdl' not found, did you extract all vanilla files from all VPKs?"
+	exit 1
+fi
+
+mapfile -t astrFl < <(ls -1 "${strPath}/")
+for strFl in "${astrFl[@]}";do
+	strFlTo="bread02_cooked${strFl#l6_jar_oil}"
+	cp -vf "${strPath}/$strFl" "$strFlTo"
+done
