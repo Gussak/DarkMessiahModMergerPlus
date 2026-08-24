698ea4abdb3f49d45f66d0cfa1cca608b30585d1
gussak <ghussack@gmail.com>
2026-08-24 04:52:05 -0300

    fix

--- _mods/MultiplyEnemies/content/cfg/createNpcSpawnSwitches.sh
+++ _mods/MultiplyEnemies/content/cfg/createNpcSpawnSwitches.sh
@@ -391,7 +391,7 @@ function FUNCappendToSpawnTrigger() {
 			if((lnSTTemplateIndex==nSpawnTriggerLinkedLimit));then #not greater than limit because the last slot will be used to link the next section
 				#lnSTTemplateBeginSection=$lnSTTemplateIndex
 				#lstrSTSectionID="gskSpawnerSectionBegin${lnSTTemplateBeginSection}"
-				lstrSTSectionID="gskSpawnerSectionBeginAtTarget$((liTargetIndex+1))"
+				lstrSTSectionID="gskSpawnerSectionBeginAtTarget${liTargetIndex}"
 				lstrTargetName="$lstrSTSectionID"
 				lbCreateNewSection=true
 			else
