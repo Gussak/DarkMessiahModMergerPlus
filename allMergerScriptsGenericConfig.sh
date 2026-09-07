5601632b7bd4b49011b33bed7815a2a4e0013c30
gussak <ghussack@gmail.com>
2026-09-07 05:07:11 -0300

    fix

--- allMergerScriptsGenericConfig.sh
+++ allMergerScriptsGenericConfig.sh
@@ -94,7 +94,7 @@ FUNCchkDeps() {
 FUNCaskYesNo() { # <questionForYesNo>. use like: if FUNCaskYesNo "oi?";then ...
 	while read -t 0.1 -n 1;do :;done #clear key buffer
 	local lstrResp
-	echo -n "${1}? (y/...)" >&2
+	echo -n "[QUESTION] ${1}? (y/...)" >&2
 	read -n 1 lstrResp&&:;echo
 	if [[ "$lstrResp" =~ [yY] ]];then return 0;fi
 	return 1
