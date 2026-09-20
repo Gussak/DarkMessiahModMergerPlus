fe7d277556385ecfd2ef457382ebca175d17ab07
gussak <ghussack@gmail.com>
2026-09-20 14:19:31 -0300

    fix

--- prepareAllModsPatchesForScriptFile.sh
+++ prepareAllModsPatchesForScriptFile.sh
@@ -33,7 +33,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-set -x
+#set -x
 #set -Eeu;if true;then #BUGTRACK_OVERKILL
 
 export FUNCminiModInit_bConsumeParamHelp=false
